77892f0e0a8814e4a26b00b2da15aed5159d31f8
Joel Ramirez <joelramirez@MacBook-Pro-5.home>
2019-05-12 09:34:26 -0400

    connection setup

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const connection = require('./db/connection');
+const path = require('path');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  connection.query("SELECT * FROM noteBook", function (err, notes) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json(notes);
+  });
+});
+
+app.post("/api/notes", function (req, res) {
+  connection.query("INSERT INTO noteBook SET ?", req.body, function (err, result) {
+    if (err) return res.status(500).json(err);
+
+    res.json({ status: 'successful' });
+  });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  connection.query("DELETE FROM noteBook WHERE id = ?", [req.params.id], function (err, result) {
+    if (err) res.status(500).end();
+
+    res.json(result);
+  })
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
